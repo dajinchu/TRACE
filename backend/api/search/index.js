7a7c30aa5e851e15bb585a8241c1bb9e59471bf3
Da-Jin Chu <dajinchu@gmail.com>
2019-04-06 17:35:14 -0400

    zeit stuff

---
+++ backend/api/search/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const app = express();
+module.exports = app;
+
+app.get('*', (req, res) => {
+  res.json([
+    {
+      type: "course",
+      uid: "82910",
+      code: "CS 3500",
+      name: "Object Oriented Design",
+      profs: [
+        {
+          name: "Ben Lerner",
+          effectiveness: 5,
+          personality: 5,
+          challenge: 5,
+          workload: 5,
+          learning: 5,
+          overall: 5,
+        },
+        {
+          name: "Clark Freifeld",
+          effectiveness: 4,
+          personality: 4,
+          challenge: 4,
+          workload: 4,
+          learning: 4,
+          overall: 4,
+        }
+      ]
+    },
+    {
+      type: "prof",
+      uid: "9201",
+    }
+  ]);
+});
