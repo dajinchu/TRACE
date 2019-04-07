a893332f1bdf215ef23dad3bffaf8e0622f58893
Da-Jin Chu <dajinchu@gmail.com>
2019-04-07 03:18:36 -0400

    prof and mock index

---
+++ backend/api/prof/index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const cors = require('cors')
+
+const app = express();
+app.use(cors())
+module.exports = app;
+
+app.get('*', (req, res) => {
+  res.json({
+    UID: "81202",
+    name: "Ben Lerner",
+    avg: {"lecture":4.46,"workload":17.75,"personality":4,"overall":4.56,"challenge":4.46,"learning":4.43},
+    courses: [
+      {
+        UID: 8129,
+        name: "Object Oriented Design",
+        semesters: [
+          {
+            UID: 87,
+            name: "Fall 2018",
+            metrics: {
+              "lecture": 4,
+              "workload": 20,
+              "personality":4,
+              "overall":4.5,
+              "challenge":4.4,
+              "learning":4.2
+            }
+          },
+          {
+            UID: 83,
+            name: "Spring 2018",
+            metrics: {
+              "lecture": 4.2,
+              "workload": 15,
+              "personality":4.1,
+              "overall":4.4,
+              "challenge":4.2,
+              "learning":4.5
+            }
+          }
+        ]
+      },
+      {
+        UID: 2912,
+        name: "Fundamentals of Computer Science 1",
+        semesters: [
+          {
+            UID: 21,
+            name: "Spring 2016",
+            metrics: {
+              "lecture": 4.2,
+              "workload": 15,
+              "personality":4.1,
+              "overall":4.4,
+              "challenge":4.2,
+              "learning":4.5
+            }
+          }
+        ]
+      }
+    ]
+  });
+});
--- backend/api/search/index.js
+++ backend/api/search/index.js
@@ -6,37 +6,42 @@ app.use(cors())
 module.exports = app;
 
 app.get('*', (req, res) => {
-  res.json([
-    {
-      type: "course",
-      uid: "82910",
-      code: "CS 3500",
-      name: "Object Oriented Design",
-      profs: [
-        {
-          name: "Ben Lerner",
-          effectiveness: 5,
-          personality: 5,
-          challenge: 5,
-          workload: 5,
-          learning: 5,
-          overall: 5,
-        },
-        {
-          name: "Clark Freifeld",
-          effectiveness: 4,
-          personality: 4,
-          challenge: 4,
-          workload: 4,
-          learning: 4,
-          overall: 4,
-        }
-      ]
-    },
-    {
-      type: "prof",
-      name: "Matthias Felleisen",
-      uid: "9201",
-    }
-  ]);
+  query = req.query.q;
+  if (query == ''){
+    res.json([]);
+  } else {
+    res.json([
+      {
+        type: "course",
+        uid: "82910",
+        code: "CS 3500",
+        name: "Object Oriented Design",
+        profs: [
+          {
+            name: "Ben Lerner",
+            effectiveness: 5,
+            personality: 5,
+            challenge: 5,
+            workload: 5,
+            learning: 5,
+            overall: 5,
+          },
+          {
+            name: "Clark Freifeld",
+            effectiveness: 4,
+            personality: 4,
+            challenge: 4,
+            workload: 4,
+            learning: 4,
+            overall: 4,
+          }
+        ]
+      },
+      {
+        type: "prof",
+        name: "Matthias Felleisen",
+        uid: "9201",
+      }
+    ]);
+  }
 });
