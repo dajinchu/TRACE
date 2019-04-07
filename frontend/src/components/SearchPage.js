4a2f893b55157f5cc12c1511877b58d71a8c06be
Ryan Drew <ryan.drew14@gmail.com>
2019-04-06 21:07:17 -0400

    add global style, style header, setup layout for search page

---
+++ frontend/src/components/SearchPage.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+export const BACKEND_BASE_URL = 'https://trace.dajinchu.now.sh/backend/api';
+export const SEARCH_ITEM_TYPES = {
+  PROF: 'prof',
+  COURSE: 'course',
+}
+
+const SearchPageContainer = styled.div`
+  background-color: #B12E2A;
+  height: 100vh;
+`;
+
+const Title = styled.h1`
+  font-weight: 500;
+  font-size: 64px;
+  letter-spacing: .1em;
+  text-transform: uppercase;
+  padding: 65px 14%;
+  line-height: normal;
+  color: #fff;
+`;
+
+const SearchBar = styled.input`
+  
+`;
+
+const SearchItemsContainer = styled.div`
+  margin: 50px 20%;
+`;
+
+const SearchPage = (props) => {
+  const [query, setQuery] = useState('');
+  const [searchItems, setSearchItems] = useState([])
+
+  useEffect(() => {
+    fetch(
+      `${BACKEND_BASE_URL}/search?q=${query}`,
+    ).then(response => {
+      response.json().then((data) => {
+        setSearchItems(data);
+      });
+    }).catch(error => console.error(error));
+    
+    console.log(searchItems)
+  }, [query]);
+
+  return (
+    <SearchPageContainer>
+      <Title>
+        SEARCH<br/>TRACE.
+      </Title>
+      <SearchBar
+        type="text"
+        value={query}
+        onChange={e => setQuery(e.target.value)}
+      />
+      <SearchItemsContainer>
+        {searchItems.map(searchItem => {
+          if (searchItem.type === SEARCH_ITEM_TYPES.PROF) {
+            return null; // PROFITEM
+          } else if (searchItem.type === SEARCH_ITEM_TYPES.COURSE) {
+            return null; // COURSEITEM
+          } else {
+            console.error(`Unrecognized search item type "${searchItem.type}".`)
+            return null;
+          }
+        })}
+      </SearchItemsContainer>
+    </SearchPageContainer>
+  );
+};
+
+export default SearchPage;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import SearchPage from './components/SearchPage';
 import * as serviceWorker from './serviceWorker';
+import GlobalStyle, { CSSReset } from './GlobalStyle'
 
-ReactDOM.render(<SearchPage />, document.getElementById('root'));
+const App = () => {
+  return (
+    <>
+      <CSSReset />
+      <GlobalStyle />
+      <SearchPage />
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
