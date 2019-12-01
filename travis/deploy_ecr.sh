e0fee0485c1498b9105a1f9ead5a90faa73ccd59
Da-Jin Chu <dchu@alignable.com>
2019-12-01 01:18:30 -0500

    script plss

--- travis/deploy_ecr.sh
+++ travis/deploy_ecr.sh
@@ -1,7 +1,9 @@
+cd $1
+
 docker --version
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 eval $(aws ecr get-login --no-include-email --region us-east-1)
 docker build -t tracedemo-$1:$TRAVIS_BUILD_ID .
-docker tag tracedemo-$1:$TRAVIS_BUILD_ID $AWS_ECR_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/tracedemo-$1:$TRAVIS_BUILD_ID
-docker push $AWS_ECR_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/tracedemo-$1:$TRAVIS_BUILD_ID
+docker tag tracedemo-$1:$TRAVIS_BUILD_ID $AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/tracedemo-$1:$TRAVIS_BUILD_ID
+docker push $AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/tracedemo-$1:$TRAVIS_BUILD_ID
