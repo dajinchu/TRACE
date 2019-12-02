98b2f607f0f10ac6c6dad838fab486cc076ad38a
Da-Jin Chu <dchu@alignable.com>
2019-12-02 13:48:41 -0500

    docker push latest tag

--- travis/deploy_ecr.sh
+++ travis/deploy_ecr.sh
@@ -6,4 +6,6 @@ export PATH=$PATH:$HOME/.local/bin
 eval $(aws ecr get-login --no-include-email --region us-east-1)
 docker build -t tracedemo-$1:$TRAVIS_BUILD_ID .
 docker tag tracedemo-$1:$TRAVIS_BUILD_ID $AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/tracedemo-$1:$TRAVIS_BUILD_ID
+docker tag tracedemo-$1:$TRAVIS_BUILD_ID $AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/tracedemo-$1:latest
 docker push $AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/tracedemo-$1:$TRAVIS_BUILD_ID
+docker push $AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/tracedemo-$1:latest
