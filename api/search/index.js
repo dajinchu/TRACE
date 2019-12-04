b4d0a5db0be612f435602ef1bbb7d7b09b9edc3d
dajinchu <dajinchu@gmail.com>
2019-12-03 20:22:45 -0500

    remove extensions

--- api/search/index.js
+++ api/search/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const request = require('request-promise-native');
-const jwtCheck = require('../auth.js');
+const jwtCheck = require('../auth');
 const router = express.Router();
 
 router.use(jwtCheck);
